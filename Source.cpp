dd5fd8882ef4ffe0ac4b7b4c9e53e024708139cf
lonelyuber <35495442+lonelyuber@users.noreply.github.com>
2018-01-24 10:00:32 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int input;
+
+	cout << "What's your score?" << endl;
+	cin >> input;
+	if (input < 50)
+		cout << "It's okay next time buddy :)" << endl;
+	else if (input > 50)
+		cout << "gOoD jOb BuDdYyYyY :)))" << endl;
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+}
